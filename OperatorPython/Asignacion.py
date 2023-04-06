884b88ea114c1310425e1b0a729e92711607d7e6
JrGustavo <gus@soypetal.com>
2023-04-06 12:32:31 -0500

    Update Course Python_ University

---
+++ OperatorPython/Asignacion.py
@@ -0,0 +1,11 @@
+miVariable = 10
+print(miVariable)
+
+miVariable = miVariable + 1
+
+miVariable += 1
+print(miVariable)
+
+miVariable -= 2
+print(miVariable)
+
---
+++ OperatorPython/Numeros.py
@@ -0,0 +1,7 @@
+a = int(input('Escribe un valor numerico:  '))
+
+print(a % 2)
+if a % 2 == 0:
+    print(f' El valor de a {a} es nùmero par ')
+else:
+    print(f' El valor de a {a} es nùmero impar ')
